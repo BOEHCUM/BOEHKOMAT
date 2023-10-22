01b058480e2333f9e608491b14cf384e7e44a19f
BOEHKOMAT <145224407+BOEHCUM@users.noreply.github.com>
2023-10-22 16:28:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+VANTA.CELLS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  color1: 0xff0000,
+  color2: 0x0,
+  size: 0.20
+})
